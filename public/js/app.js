6a805e61d609291c7a780d98ba4ea0f5edaf2311
Great Louise <temenecoinc@Louise.local>
2021-03-02 00:15:03 +0800

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 
 
- const weatherform = document.querySelector('form')
+const weatherform = document.querySelector('form')
  const search = document.querySelector('input')
  const messageOne = document.querySelector('#message-1')
  const messageTwo = document.querySelector('#message-2')
@@ -18,7 +18,7 @@
     
 
     
-     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+     fetch('/weather?address=' + location).then((response) => {
      response.json().then((data) => {  
          if(data.error) {
             messageOne.textContent = data.error
